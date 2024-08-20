c1cf11c20669e683858ebc11b9ec899cb0b2dc18
quiet-web <qureshiammar900@gmail.com>
2024-08-19 22:21:44 -0700

    oops concept

---
+++ python.py
@@ -0,0 +1,13 @@
+class Employee :
+   
+    def __init__(self,name ,salary,extra_hours=50) :
+        self.name =  name
+        self.salary = salary
+        self.extra_hours = extra_hours
+    def display(self):
+        return (self.salary)
+
+
+name:str = str(input("Enter name : "))  
+salary:int = int(input("Enter the salary : "))
+  
